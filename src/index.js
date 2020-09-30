2375beb974172924da30a4385fdbb6b21d361a10
Svetlana Borovitskaia <borovitzki@gmail.com>
2020-09-30 15:06:14 +0300

    feat: implement max fonction

--- src/index.js
+++ src/index.js
@@ -13,7 +13,16 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
+    if (typeof array === 'undefined' || array.length === 0) {
   return 0;
+}
+    let max = array[0] 
+    array.forEach(number => {
+        if (max < number) {
+            max = number
+        }
+    })
+    return max;
 }
 
 exports.avg = function avg (array) {
