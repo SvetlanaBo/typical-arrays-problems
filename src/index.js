bd3d8903192d58cca1646a5bb7382960c2ce5236
Svetlana Borovitskaia <borovitzki@gmail.com>
2020-09-30 15:05:33 +0300

    feat: implement min function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 
 exports.min = function min (array) {
+    if (typeof array === 'undefined' || array.length === 0) {
   return 0;
+}
+        let min = array[0] 
+    array.forEach(number => {
+        if (min > number) {
+            min = number
+        }
+    })
+  return min;
 }
 
 exports.max = function max (array) {
